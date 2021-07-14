6b8c2d45e6814dd08005cc2cfef9299814b1948b
Akhil Mohan <akhil.mohan@mayadata.io>
2021-07-14 21:14:34 +0530

    add method signatures
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

---
+++ src/lvm.rs
@@ -0,0 +1,17 @@
+use std::error::Error;
+
+fn create_vg()  {
+    todo!()
+}
+
+fn remove_vg() {
+    todo!()
+}
+
+fn create_lvm_vol() {
+    todo!()
+}
+
+fn remove_lvm_vol() {
+    todo!()
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+mod lvm;
+
+fn main() {}
