59e419e6326e9d299c3680ed158ae525d9c3522d
shubham <shubham.bajpai@mayadata.io>
2021-07-19 21:05:38 +0530

    Add support for vgcreate command
    $ sudo ./target/debug/lib-lvm vgcreate -h
    lib-lvm-vgcreate
    Create volume group
    
    USAGE:
        lib-lvm vgcreate <name> <disks>...
    
    FLAGS:
        -h, --help       Prints help information
        -V, --version    Prints version information
    
    ARGS:
        <name>        Volume group name
        <disks>...    Disk device files
    
    Sample output:
    $ sudo ./target/debug/lib-lvm vgcreate tpool /dev/sdb
    Ok(
        Pool {
            name: "tpool",
            devices: [
                "/dev/sdb",
            ],
            capacity: 15372124160,
            used: 0,
        },
    )
    
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- src/lvm.rs
+++ src/lvm.rs
@@ -1,7 +1,51 @@
 use std::error::Error;
+use std::process::Command;
 
-fn create_vg()  {
-    todo!()
+#[derive(Debug)]
+pub struct PoolCreateReq {
+    pub name: String,
+    pub devices: Vec<String>,
+}
+
+#[derive(Debug)]
+pub struct Pool {
+    pub name: String,
+    pub devices: Vec<String>,
+    pub capacity: u64,
+    pub used: u64,
+}
+
+
+
+pub(crate) fn create_vg(req: PoolCreateReq) -> Result<Pool, Box<dyn Error>> {
+    let pool_name = req.name.as_str();
+    let output = Command::new("pvcreate")
+        .args(&req.devices)
+        .output()
+        .expect("failed execute pv_create");
+    // TODO: check output and return custom error 
+    let output = Command::new("vgcreate")
+        .arg(pool_name)    
+        .args(&req.devices)
+        .output()
+        .expect("failed execute vg_create");
+     // TODO: check output and return custom error 
+    let output = Command::new("vgs")
+        .arg(pool_name)
+        .args(&["--options=vg_size,vg_free", "--units=b", "--noheadings", "--nosuffix"])
+        .output()
+        .expect("failed execute vgs");
+    let s = std::str::from_utf8(output.stdout.as_slice())?;
+    let size = s.trim().split(" ").collect::<Vec<&str>>();
+    let capacity = size[0].parse::<u64>()?;
+    let free = size[1].parse::<u64>()?;
+    let pool = Pool{
+        name: pool_name.to_string(),
+        devices: req.devices,
+        capacity: capacity,
+        used: capacity - free,
+    };
+    Ok(pool)
 }
 
 fn remove_vg() {
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,70 @@
-mod lvm;
+use lvm::{Pool, PoolCreateReq};
 
-fn main() {}
+use snafu::{Snafu};
+use clap::{App, Arg, ArgMatches};
+
+pub mod lvm;
+
+#[derive(Debug, Snafu)]
+pub enum Error {
+    #[snafu(display("Missing value for {}", field))]
+    MissingValue { field: String },
+}
+
+fn main() {
+
+    let matches = App::new("lvm-lib")
+        .version("0.1")
+        .about("lvm commands to create pools and volumes")
+        .subcommand(App::new("vgcreate")
+            .about("Create volume group")
+            .arg(
+                Arg::with_name("name")
+                    .required(true)
+                    .index(1)
+                    .help("Volume group name"),
+            )
+            .arg(
+                Arg::with_name("disks")
+                    .required(true)
+                    .multiple(true)
+                    .index(2)
+                    .help("Disk device files"),
+            )
+        )
+        .get_matches();
+
+    let status = match matches.subcommand() {
+        ("vgcreate", Some(args)) => {
+            let pool = create(args);
+            println!("{:#?}", pool)
+        },
+        _ => panic!("Command not found"),
+        
+    };
+
+}
+
+fn create(
+    matches: &ArgMatches<'_>,
+) -> Result<lvm::Pool, Box<dyn std::error::Error>> {
+    let name = matches
+        .value_of("name")
+        .ok_or_else(|| Error::MissingValue {
+            field: "name".to_string(),
+        })?
+        .to_owned();
+    let disks = matches
+        .values_of("disks")
+        .ok_or_else(|| Error::MissingValue {
+            field: "disks".to_string(),
+        })?
+        .map(|dev| dev.to_owned())
+        .collect();
+    let mut req = PoolCreateReq{
+            name: name,
+            devices: disks,
+        };
+    let pool = lvm::create_vg(req)?;
+    Ok(pool)
+}
